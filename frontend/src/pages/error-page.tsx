5b1933a061d291297fe07cdb8717e689bfa036e2
Ansh Shukla <contact@anshshukla.dev>
2023-08-14 17:39:25 +0300

    hotfix: router error typing

--- frontend/src/pages/error-page.tsx
+++ frontend/src/pages/error-page.tsx
@@ -1,7 +1,10 @@
 import { useRouteError } from "react-router-dom";
 
 export const ErrorPage = () => {
-  const error = useRouteError();
+  const error = useRouteError() as {
+    statusText?: string;
+    message?: string;
+  };
 
   return (
     <div id="error-page">
